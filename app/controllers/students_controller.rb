47ffa59c213c7ab7e84cd853f24c923936c450b9
Jess Rezac <jess.rezac@gmail.com>
2019-09-25 13:37:54 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+  
+  :new, :create, :show, :edit, :update
+  
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+end
