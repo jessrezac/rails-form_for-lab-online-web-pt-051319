0f5cf612fcec3f2e72b4c9a2bca3aab4a20d52fe
Jess Rezac <jess.rezac@gmail.com>
2019-09-25 14:44:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -21,7 +21,6 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
-
   end
   
   private
